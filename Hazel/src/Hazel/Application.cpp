813207078ad33312f9aff2416cb942e70feefa63
Shivasankar-1996 <69097507+Shivasankar-1996@users.noreply.github.com>
2022-02-12 00:49:12 +0530

    Entry Point

---
+++ Hazel/src/Hazel/Application.cpp
@@ -0,0 +1,23 @@
+#include "Application.h"
+
+namespace Hazel {
+
+	Application::Application() {
+
+	}
+
+	Application::~Application() {
+
+	}
+
+
+
+	void Application::Run() {
+		std::cout << "Welcome to Hazel!!!!!" << std::endl;
+
+		while (1) {
+
+		}
+	}
+
+}
