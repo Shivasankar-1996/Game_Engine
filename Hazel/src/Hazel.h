1d78c76ae85c8c5fb25a1ea53c518a651a019e3f
Shivasankar-1996 <69097507+Shivasankar-1996@users.noreply.github.com>
2022-05-11 19:55:01 +0530

    Premake added

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -1,8 +1,9 @@
 #pragma once
 
+#include"Hazel\Log.h"
 #include"Hazel\Application.h"
 
-#include"Hazel\Log.h"
+
 
 
 #include"Hazel\EntryPoint.h"
--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -7,6 +7,7 @@ extern Hazel::Application* Hazel::CreateApplication();
 int main(int argc, char** argv) {
 
 	Hazel::Log::Init();
+	//Hazel::Log::GetCoreLogger()->warn("Initialized Log!");
 	HZ_CORE_WARN("Initialized Log!");
 	int a = 5;
 	HZ_INFO("Hello! Var={0}", a);
